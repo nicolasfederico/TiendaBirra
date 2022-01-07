f039a2f053c35a037cf96acfe4f951b7a20b07fe
Defelippekevin <kevin.defelippe@gmail.com>
2022-01-07 19:06:27 -0300

    subo modulo codigoDescuento para revisar

--- proyecto-birra/src/codigo-descuento/codigo-descuento.controller.ts
+++ proyecto-birra/src/codigo-descuento/codigo-descuento.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { CodigoDescuentoService } from './codigo-descuento.service';
+import { codigoDescuentoDTO } from './codigoDescuento.dto';
+import { CodigoDescuento } from './codigoDescuento.entity';
 
 @Controller('codigo-descuento')
-export class CodigoDescuentoController {}
+export class CodigoDescuentoController {
+
+    constructor(private readonly codigoDescuentoService: CodigoDescuentoService){}
+
+
+    @Get()
+    public async getCodigosDescuentos():Promise<CodigoDescuento[]>{
+        return await this.codigoDescuentoService.getCodigosDescuentos();
+    }
+
+    
+    @Get(':id')
+    public async getCodigoDescuento(@Param('id') idCodigoDescuento:string):Promise<CodigoDescuento>{
+        return await this.codigoDescuentoService.getCodigoDescuento(idCodigoDescuento);
+    }
+
+
+    @Post('')
+    public async addCodigoDescuento(@Body() codigoDescuento:codigoDescuentoDTO): Promise<CodigoDescuento>{
+        return await this.codigoDescuentoService.postCodigoDescuento(codigoDescuento);
+    }
+
+
+    @Delete(':id')
+
+    public async deleteUsuario(@Param('id') id:string) : Promise<CodigoDescuento>{
+        return await this.codigoDescuentoService.deleteCodigoDescuento(id);
+    }
+    
+    @Put(':id')
+     public async putUsuario(@Param('id') id:string, @Body() codigoDescuento:codigoDescuentoDTO): Promise<CodigoDescuento>{
+         return await this.codigoDescuentoService.putCodigoDescuento(id,codigoDescuento);
+     }
+}
--- proyecto-birra/src/codigo-descuento/codigo-descuento.module.ts
+++ proyecto-birra/src/codigo-descuento/codigo-descuento.module.ts
@@ -1,18 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FacturaController } from 'src/factura/factura.controller';
 import { Factura } from 'src/factura/factura.entity';
-import { FacturaService } from 'src/factura/factura.service';
-import Usuario from 'src/usuario/usuario.entity';
 import { CodigoDescuentoController } from './codigo-descuento.controller';
 import { CodigoDescuentoService } from './codigo-descuento.service';
 import { CodigoDescuento } from './codigoDescuento.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([CodigoDescuento,Usuario,Factura])
-    ],
-    controllers: [CodigoDescuentoController,FacturaController],
-  providers: [CodigoDescuentoService,FacturaService]
+    TypeOrmModule.forFeature([CodigoDescuento,Factura])],
+    controllers: [CodigoDescuentoController],
+  providers: [CodigoDescuentoService]
 })
 export class CodigoDescuentoModule {}
