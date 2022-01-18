938d5651aff79293e9fe262bf51e0577cf5b9968
nicolasfederico <nicolasfmertens@gmail.com>
2022-01-18 19:34:30 -0300

    subida registro

---
+++ proyecto-birra/client/js/registro.js
@@ -0,0 +1,38 @@
+let nombreRegistro = document.querySelector("#nombreRegistro");
+let apellidoRegistro = document.querySelector("#apellidoRegistro");
+let dniRegistro = document.querySelector("#dniRegistro");
+let direccionRegistro = document.querySelector("#direccionRegistro");
+let mailRegistro = document.querySelector("#mailRegistro");
+let telRegistro= document.querySelector("#telRegistro");
+let passwordRegistro= document.querySelector("#passwordRegistro");
+
+let btnRegistro = document.querySelector("#btn-registro");
+
+
+btnRegistro.addEventListener ("click", ()=> {
+    let user={
+        "nombre":nombreRegistro.value,
+        "apellido": apellidoRegistro.value,
+        "dni":dniRegistro.value,
+        "direccion": direccionRegistro.value,
+        "mail": mailRegistro.value,
+        "admin": false,
+        "telefono": telRegistro.value,
+        "password": passwordRegistro.value
+    };
+    crear (user)
+} )
+
+async function crear(user){
+    
+    
+    let response = await fetch("/usuario",{
+        "method": "POST",
+        "mode": 'cors',
+        "headers": {
+            'Content-Type': 'application/json',
+        },
+        "body": JSON.stringify(user)
+    });
+    let r = await response.json();
+}
