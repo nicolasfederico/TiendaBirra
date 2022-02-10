4372e6a914ed468d7f4db8a5e1052c3e55d533a0
nicolasfederico <nicolasfmertens@gmail.com>
2022-02-10 20:13:30 -0300

    detalle carrito con modificacion de cantidad funcionando, falta hacer el boton de compra y demasd

--- proyecto-birra/client/js/carrito.js
+++ proyecto-birra/client/js/carrito.js
@@ -3,6 +3,7 @@ let detalles = [];
 let params = [];
 
 let totalCarrito = document.querySelector('#totalCarrito')
+let error = document.querySelector('#error')
 
 
 
@@ -18,9 +19,85 @@ function processParams(){
 
 async function loadCarrito(){
     try {
+        
+        let total =0;
+        let idCarrito = processParams()["index"];
+
+        let response = await fetch (`/detalle-carrito/${idCarrito}`);
+        
+        if (response.ok) {
+            console.log(response.ok)
+            let t = await response.json()
+            detalles = t            
+            datosCarrito.innerHTML = "";
+            totalCarrito.innerHTML = "";
+            for (let i=0; i<=detalles.length; i++) {
+                let nombre = detalles[i].producto.nombre;
+                let idProducto = detalles[i].producto.id_producto;
+                let cantidad = detalles[i].cantidad;
+                let precio = detalles[i].producto.precio;
+                let subtotal = (cantidad*precio)
+
+                datosCarrito.innerHTML+=`
+                <tr>
+                <td>${nombre}</td>
+                <td><input value="${cantidad}" id="inputCantidad-${i}" onChange="cambiarCantidad(${i},${idProducto},${idCarrito})"></input></td>
+                <td>${precio}</td>
+                <td id="subtotal">${precio*cantidad}</td>
+                </tr>`
+
+                totalCarrito.innerHTML = "";
+                totalCarrito.innerHTML +=`
+                El total es: ${total+=subtotal}`
+            }            
+        }
+        else {
+            container.innerHTML = "<h1> Error - Failed URL!</h1>";
+        }
+    }
+    catch (err) {
+        container.innerHTML = "<h1>"+ err.message+ "error</h1>";
+    };
+}
+
+
+
+loadCarrito ();
+
+async function cambiarCantidad(i,idProducto,idCarrito){
+    let cantidad = document.querySelector(`#inputCantidad-${i}`)
+    console.log (idCarrito)
+    console.log (idProducto)
+    let cantidadEnInput = cantidad.value;
+    console.log (cantidadEnInput)
+   
+    try {
+        let detalleCarrito={
+            "id_producto": idProducto,
+            "cantidad": cantidadEnInput,
+        }
+        let response = await fetch (`/detalle-carrito/${idCarrito}/${idProducto}`,{
+              'method': 'PUT',
+              'headers': {'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(detalleCarrito)
+      });
+      if (response.ok) {
+         loadCarrito();
+      } else{
+          alert("error");
+      }
+  } catch (error) {
+      alert("error y capturado en el catch");
+  }
+
+    datosCarrito.innerHTML = "";
+
+    try {
         let total =0;
         let params = processParams();
         let response = await fetch (`/detalle-carrito/${params["index"]}`);
+        let idCarrito = params["index"];
         if (response.ok) {
             let t = await response.json()
             detalles = t
@@ -29,9 +106,11 @@ async function loadCarrito(){
             datosCarrito.innerHTML = "";
             totalCarrito.innerHTML = "";
             //producto cantidad precio
+            
 
             for (let i=0; i<=detalles.length; i++) {
                 let nombre = detalles[i].producto.nombre;
+                let idProducto = detalles[i].producto.id_producto;
                 let cantidad = detalles[i].cantidad;
                 let precio = detalles[i].producto.precio;
                 let subtotal = (cantidad*precio)
@@ -39,7 +118,7 @@ async function loadCarrito(){
                 datosCarrito.innerHTML+=`
                 <tr>
                 <td>${nombre}</td>
-                <td><input value="${cantidad}" id="inputCantidad" onChange="putSubtotal(${inputCantidad.value},${precio})"></input></td>
+                <td><input value="${cantidad}" id="inputCantidad-${i}" onChange="cambiarCantidad(${i},${idProducto},${idCarrito})"></input></td>
                 <td>${precio}</td>
                 <td id="subtotal">${precio*cantidad}</td>
                 </tr>`
@@ -50,20 +129,15 @@ async function loadCarrito(){
             }            
         }
         else
-            container.innerHTML = "<h1> Error - Failed URL!</h1>";
+        container.innerHTML = "<h1> Error - Failed URL!</h1>";
     }
     catch (err) {
         container.innerHTML = "<h1>"+ err.message+ "error</h1>";
     };
+
+    
+
+    
+
 }
-
-
-
-function putCantidad (cantidad,precio) {
-    let inputCantidad=document.querySelector("#inputCantidad");
-    subtotal = (cantidad*precio);
-    return subtotal;    
-}
-
-loadCarrito ();
 
