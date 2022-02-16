7cb3c3177d0361299886218eae769d495fc740fe
nicolasfederico <nicolasfmertens@gmail.com>
2022-02-16 16:44:19 -0300

    Trivia y cupón funcionando correctamente

--- proyecto-birra/client/js/triviaCupon.js
+++ proyecto-birra/client/js/triviaCupon.js
@@ -2,40 +2,18 @@ let codigoDescuento = document.querySelector('#input-CodigoDescuento')
 let inputDescuento = document.querySelector('#input-descuento');
 let preguntaTrivia = document.querySelector('#input-pregunta-trivia');
 let respuestaTrivia = document.querySelector('#respuestaTrivia')
-// let respuestaTriviaFalse = document.querySelector('#input-frespuesta-trivia');
-// let respuestaTriviaTrue = document.querySelector('#input-vrespuesta-trivia');
 let btnGuardar = document.querySelector('#btn-guardar-descuentoTrivia')
 
 btnGuardar.addEventListener ("click", async function(e){
-    let descuento={
+    let trivia={
+        "pregunta": preguntaTrivia.value,
+        "respuesta": respuestaTrivia.value,
         "id_codigo_descuento":codigoDescuento.value,
         "descuento": inputDescuento.value,
         "activo":true
     }
-    console.log(respuestaTrivia.value)
-    let trivia= {
-        "pregunta": preguntaTrivia.value,
-        "respuesta": respuestaTrivia.value,
-        "id_codigo_descuento": codigoDescuento.value
-    }
-    
-    crearDescuento (descuento)
     crearTrivia(trivia)
 } )
-
-
-async function crearDescuento(descuento){
-    
-    let response = await fetch('/codigo-descuento',{
-        "method": "POST",
-        "mode": 'cors',
-        "headers": {
-            'Content-Type': 'application/json',
-        },
-        "body": JSON.stringify(descuento)
-    });
-    let r = await response.json();
-}
 
 async function crearTrivia(trivia){
     
--- proyecto-birra/src/trivia/trivia.dto.ts
+++ proyecto-birra/src/trivia/trivia.dto.ts
@@ -2,4 +2,6 @@ export class TriviaDTO{
     readonly pregunta:string;
     readonly respuesta:boolean;
     readonly id_codigo_descuento:string;
+    readonly descuento:number;
+    readonly activo:boolean;
 }
